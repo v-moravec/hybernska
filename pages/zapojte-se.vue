6922bd43add0054acda2df61c8d244c48aa837e0
Vojtěch Moravec <voj.moravec@seznam.cz>
2022-12-11 18:00:30 +0100

    Updated join page.

--- pages/zapojte-se.vue
+++ pages/zapojte-se.vue
@@ -9,24 +9,40 @@
         Pojďte s námi ukázat váš potenciál, získat další nové zkušenosti i kontakty a připsat si další řádek do vašeho CV. 😉
       </p>
       <p>
-        V tomto článku se dozvíte, co po vás všechno požadujeme. Ať už se chcete stát členem týmu, nebo zadat svůj projekt. Konkrétní požadavky pak naleznete na stránkách jednotlivých projektů.
+        V tomto článku se dozvíte, co po vás všechno požadujeme. Ať už se chcete stát členem týmu, nebo zadat svůj projekt. Konkrétní požadavky pak naleznete na <NuxtLink class="underline" to="/projekty">stránkách jednotlivých projektů</NuxtLink>.
       </p>
     </div>
     <div class="p-8 mb-8 bg-black text-white rounded-3xl">
-      <h2>Kdo se může zapojit do práce na projektu?</h2>
+      <h2>Kdo se může zapojit do práce na projektu? 🤝</h2>
       <p class="mb-4">
-        Naší snahou je propojit co nejvíce studentů z různých univerzit a fakult. Tedy i vy musíte být studentem a potvrdit (např. ISIC / studentská karta, potvrzení o studiu). Další požadavky jsou uvedeny v následujícím seznamu:
+        Naší snahou je propojit co nejvíce studentů z různých univerzit a fakult. Tedy i vy <strong>musíte být studentem</strong> a prokázat se pomocí studentské karty (např. ISIC). Další požadavky jsou uvedeny v následujícím seznamu:
       </p>
-      <ul class="mb-4">
-        <li>Je nutné mít alespoň minimální znalosti svého oboru.</li>
-        <li>Na projekt je důležité si umět vyhradit čas (alespoň 10 hodin v týdnu).</li>
-        <li>… (TO DO)</li>
+      <ul class="pl-5 mb-4 list-disc">
+        <li>Je nutné mít alespoň <strong>minimální znalosti</strong> svého oboru. 😉</li>
+        <li>Na projekt je důležité si umět vyhradit <strong>čas</strong>. 🕘</li>
+        <li>Umět spolupracovat v týmu, komunikovat a být prostě kamarád. 😉</li>
       </ul>
       <p class="mb-4">
-        Pojďte s námi ukázat váš potenciál, získat další nové zkušenosti i kontakty a připsat si další řádek do vašeho CV. :)
+        V případě, že se chceš zapojit do práce na jakémkoliv projektu, přejdi na tuto stránku, vyber si projekt a klikni na “Chci se přidat”. 🙂
       </p>
+    </div>
+    <div class="p-8 mb-8 bg-black text-white rounded-3xl">
+      <h2>Kdo může zadat nový projekt? 🫵🏻</h2>
+      <p class="mb-4">
+        Kdokoliv s dobrým nápadem a chutí jej realizovat. Zároveň musí mít jasnou vizi o svém projektu a jeho potřebách. Projekt však nedokáže realizovat sám a hledá k sobě parťáky.
+      </p>
+      <ul class="pl-5 mb-4 list-disc">
+        <li>Nápad <strong>musí dávat smysl</strong> (z finančního hlediska i realizovatelnosti)</li>
+        <li>Musíte doložit <strong>konkrétní požadavky na členy týmu</strong>, které hledáte (např. jejich odbornost a zaměření, formu spolupráce atd.)</li>
+      </ul>
+      <p class="mb-4">
+        V případě, že chceš vytvořit nové zadání projektu, přejdi na <NuxtLink class="underline" to="/projekty/pridat">tuto stránku</NuxtLink>.
+      </p>
+    </div>
+    <div class="p-8 mb-8 bg-black text-white rounded-3xl">
+      <h2>Máš nějaké otázky? 🙋🏻‍♂️</h2>
       <p>
-        V tomto článku se dozvíte, co po vás všechno požadujeme. Ať už se chcete stát členem týmu, nebo zadat svůj projekt. Konkrétní požadavky pak naleznete na stránkách jednotlivých projektů.
+        V tom případě dotazů nám napište na e-mail projekty@hybernska.cz
       </p>
     </div>
   </div>
