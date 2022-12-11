6f4fbb1a10190f977d4d005f5c4aa58b92a98b86
Vojtěch Moravec <voj.moravec@seznam.cz>
2022-12-11 18:02:32 +0100

    Fix mobile nav.

--- components/MobileNavigation.vue
+++ components/MobileNavigation.vue
@@ -38,9 +38,9 @@ const links = [
     ref: 'prostory'
   },
   {
-    path: '/zapojit-se',
-    text: 'Zapojit se',
-    ref: 'zapojit-se'
+    path: '/zapojte-se',
+    text: 'Zapojte se',
+    ref: 'zapojte-se'
   },
 ]
 </script>
